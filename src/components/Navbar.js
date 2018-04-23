4dbe0cbf8e0e817b2eabf8fdedf936be52f81427
Chris Hutchison <chris88@gmail.com>
2018-04-22 23:17:14 -0400

    *
    This change addresses the need by:
    
    *

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-import github from '../img/github-icon.svg'
 import logo from '../img/logo.svg'
 
 const Navbar = () => (
@@ -10,26 +9,17 @@ const Navbar = () => (
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
           <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+            <img src={logo} alt="Kaldi" style={{ width: '120px' }} />
           </figure>
         </Link>
       </div>
       <div className="navbar-start">
-        <Link className="navbar-item" to="/about">
-          Login
-        </Link>
+
       </div>
       <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
+      <Link className="navbar-item" to="/about">
+          Login
+        </Link>
       </div>
     </div>
   </nav>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,24 +3,59 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 
 export default class IndexPage extends React.Component {
+  componentDidMount () {
+    const script = document.createElement("script");
+
+    script.src = "https://comments.comment.sh/comment-sh/js/embed.min.js";
+    script.async = true;
+
+    document.body.appendChild(script);
+  }
   render() {
     const { data } = this.props
-    const { edges: posts } = data.allMarkdownRemark
 
     return (
       <section className="section">
         <div className="container">
 
-          <div className="content">
-            <h3 className="has-text-weight-bold is-size-2">Lightweight hosted comments</h3>
-          </div>
-          <nav className="intro-buttons">
-              <a className="button is-primary is-large" href="https://github.com/jgthms/bulma/releases/download/0.7.0/bulma-0.7.0.zip">
-                <span>
-                  <strong>Signup</strong>
-                </span>
-              </a>
-            </nav>
+ 
+
+            <div className="row align-items-center">
+              <div className="col-12 mx-auto col-md-12">
+                <h1 className="mb-3 bd-text-purple-bright text-center">Lightweight hosted comments</h1>
+              </div>
+            </div>
+
+
+            <div className="row align-items-center">
+              <div className="col-6 mx-auto col-md-6 order-md-2">
+                <script data-isso="https://comments.comment.sh/comment-sh" src="https://comments.comment.sh/comment-sh/js/embed.min.js"></script>
+
+                <section id="isso-thread"></section>
+              </div>
+              <div className="col-md-6 order-md-1 text-center text-md-left pr-md-5">
+                <ul>
+                  <li><h3 id="comments-written-in-markdown">Comments written in Markdown</h3>
+
+                  <p>Users can edit or delete own comments (within 15 minutes by default).
+                  Comments in moderation queue are not publicly visible before activation.</p></li>
+
+                  <li><h3 id="disqus-wordpress-import">Disqus &amp; WordPress Import</h3>
+
+                  <p>You can migrate your Disqus/WordPress comments without any hassle.</p></li>
+
+                  <li><h3 id="configurable-js-client">Configurable JS client</h3>
+
+                  <p>Embed a single JS file, 40kb (12kb gzipped) and you are done.
+                  Supports Firefox, Safari, Chrome and IE10.</p></li>
+                </ul>
+
+                <div className="d-flex flex-column flex-md-row lead mb-3">
+                  <a href="/bootstrap-material-design/docs/4.0/getting-started/download/" className="btn btn-lg btn-secondary" >Free Signup</a>
+                </div>
+              </div>
+            </div>
+
         </div>
       </section>
     )
@@ -35,23 +70,3 @@ IndexPage.propTypes = {
   }),
 }
 
-export const pageQuery = graphql`
-  query IndexQuery {
-    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
-      edges {
-        node {
-          excerpt(pruneLength: 400)
-          id
-          fields {
-            slug
-          }
-          frontmatter {
-            title
-            templateKey
-            date(formatString: "MMMM DD, YYYY")
-          }
-        }
-      }
-    }
-  }
-`
