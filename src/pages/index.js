cf0a6a47f6433ecac1c3e10d53b22353e295d9f0
chrishutchison9 <lucy2001>
2018-04-23 08:13:42 -0400

    s

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,18 +36,8 @@ export default class IndexPage extends React.Component {
               <div className="col-md-6 order-md-1 text-center text-md-left pr-md-5">
                 <ul className="leadUl">
                   <li><h3 id="comments-written-in-markdown">Comments written in Markdown</h3>
-
-                  <p>Users can edit or delete own comments (within 15 minutes by default).
-                  Comments in moderation queue are not publicly visible before activation.</p></li>
-
                   <li><h3 id="disqus-wordpress-import">Disqus &amp; WordPress Import</h3>
-
-                  <p>You can migrate your Disqus/WordPress comments without any hassle.</p></li>
-
-                  <li><h3 id="configurable-js-client">Configurable JS client</h3>
-
-                  <p>Embed a single JS file, 40kb (12kb gzipped) and you are done.
-                  Supports Firefox, Safari, Chrome and IE10.</p></li>
+                  <li><h3 id="configurable-js-client">Configurable JS client</h3></li>
                 </ul>
 
                 <div className="d-flex flex-column flex-md-row lead mb-3">
