bb2d8f0eb4ea9fcb15171b911123e23a656607c4
Chris Hutchison <chutchison@dvidshub.net>
2018-04-24 16:41:33 -0400

    s

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -10,6 +10,7 @@ const TemplateWrapper = ({ children }) => (
     <Helmet title="Comment.sh - Lightweight hosted comments">
         <meta name="keywords" content="disqus alternative, isso hosting, hosted comment" />
         <meta name="description" content="Lightweight Disqus alternative. Hosted comments powered by the Isso commenting platform." />
+        <link rel="stylesheet" href="/css/markdown.css" />
     </Helmet>
     <Navbar />
     <div>{children()}</div>
